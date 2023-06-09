01df32cbdcf42293f6a240b47e3f5e671864fd26
Piotr Matlak <pmatl@softserveinc.com>
2023-06-09 15:23:51 +0200

    setup github variables for hoisting

--- src/FirebaseConfig.ts
+++ src/FirebaseConfig.ts
@@ -6,13 +6,13 @@ import firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIEikAir8s6WBtWZFgaPa-4IQS-3ygEu0",
-  authDomain: "fir-recepies-6e571.firebaseapp.com",
-  projectId: "fir-recepies-6e571",
-  storageBucket: "fir-recepies-6e571.appspot.com",
-  messagingSenderId: "648686699427",
-  appId: "1:648686699427:web:11d738473528f6e8939016",
-  measurementId: "G-R0M19M3CQL",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
 // Initialize Firebase
